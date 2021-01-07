acba89275e65a9645fd983d64872e5aa159acdb2
Vishal Soni <50075856+vishalsoni008@users.noreply.github.com>
2021-01-07 16:05:37 +0530

    Update Garbage.java

--- java Basics/Garbage.java
+++ java Basics/Garbage.java
@@ -1,11 +1,14 @@
 public class Garbage {
     public static void main(String[] args) {
         
+        //First object created
         Garbage g1=new Garbage();
+        //Second object created
         Garbage g2=new Garbage();
-
+        
+        //Assign value null both side
         g1=null;
-
+        //GArbage collector 
         System.gc();
 
         g2=null;
